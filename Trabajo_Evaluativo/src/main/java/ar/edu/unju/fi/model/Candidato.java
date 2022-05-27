4dd10287b2ec74c8214bb405590f1a8b2b8226a2
PC <PC@MEGATRON>
2022-05-27 10:16:33 -0300

    a

---
+++ Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/model/Candidato.java
@@ -0,0 +1,56 @@
+package ar.edu.unju.fi.model;
+
+public class Candidato {
+
+private int codigo;
+private String nombre;
+private String generoMusical;
+private String descripcion;
+private int cantidadVotos;
+
+
+public Candidato(int codigo, String nombre, String generoMusical, String descripcion, int cantidadVotos) {
+	super();
+	this.codigo = codigo;
+	this.nombre = nombre;
+	this.generoMusical = generoMusical;
+	this.descripcion = descripcion;
+	this.cantidadVotos = cantidadVotos;
+}
+public int getCodigo() {
+	return codigo;
+}
+public void setCodigo(int codigo) {
+	this.codigo = codigo;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getGeneroMusical() {
+	return generoMusical;
+}
+public void setGeneroMusical(String generoMusical) {
+	this.generoMusical = generoMusical;
+}
+public String getDescripcion() {
+	return descripcion;
+}
+public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+}
+public int getCantidadVotos() {
+	return cantidadVotos;
+}
+public void setCantidadVotos(int cantidadVotos) {
+	this.cantidadVotos = cantidadVotos;
+}
+
+
+
+
+
+
+}
--- Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/model/Usuario.java
+++ Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/model/Usuario.java
@@ -6,34 +6,46 @@ public class Usuario {
 
 private String nombre;
 private String email;
-private LocalDate FechaNacimiento;
+private LocalDate fechaNacimiento;
+
 
 public Usuario(String nombre, String email, LocalDate fechaNacimiento) {
 	super();
 	this.nombre = nombre;
 	this.email = email;
-	FechaNacimiento = fechaNacimiento;
+	this.fechaNacimiento = fechaNacimiento;
 }
+
+
 public String getNombre() {
 	return nombre;
 }
+
+
 public void setNombre(String nombre) {
 	this.nombre = nombre;
 }
+
+
 public String getEmail() {
 	return email;
 }
+
+
 public void setEmail(String email) {
 	this.email = email;
 }
+
+
 public LocalDate getFechaNacimiento() {
-	return FechaNacimiento;
-}
-public void setFechaNacimiento(LocalDate fechaNacimiento) {
-	FechaNacimiento = fechaNacimiento;
+	return fechaNacimiento;
 }
 
-	
+
+public void setFechaNacimiento(LocalDate fechaNacimiento) {
+	this.fechaNacimiento = fechaNacimiento;
+}
+
 
 
 
