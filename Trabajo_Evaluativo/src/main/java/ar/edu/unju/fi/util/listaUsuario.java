4d5c1d07440b3bb5e39e960559dfb3a2e7fbcd4a
Enamuel L <Enamuel L@EmAnUeL>
2022-05-29 19:55:14 -0300

    agregue listaUsuario

--- Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/util/listaUsuario.java
+++ Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/util/listaUsuario.java
@@ -1,10 +1,21 @@
 package ar.edu.unju.fi.util;
-
 import java.util.ArrayList;
 import ar.edu.unju.fi.model.Usuario;
 
 public class listaUsuario {
+	private ArrayList<Usuario> listaUsuario; 	
 	
-	ArrayList<Usuario> listaUsuario= new ArrayList<Usuario>();
 	
+	public listaUsuario() {
+		listaUsuario= new ArrayList<Usuario>();
+	}
+
+	
+	//Metodos accesores
+	public ArrayList<Usuario> getListaUsuario() {
+		return listaUsuario;
+	}
+	public void setListaUsuario(ArrayList<Usuario> listaUsuario) {
+		this.listaUsuario = listaUsuario;
+	}
 }
