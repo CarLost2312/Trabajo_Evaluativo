21f4d99dc5f5eae3024719cf079d87bc310497e1
orochimeii <babymeyfong@gmail.com>
2022-05-29 18:15:29 -0300

    css header, footer e index

--- Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/controller/CandidatosController.java
+++ Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/controller/CandidatosController.java
@@ -1,15 +1,18 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.edu.unju.fi.util.listaCandidato;
 
 @Controller
 public class CandidatosController {
 
-@GetMapping("/Candidatos")
-public String getIndexPage() {
-	return "Candidatos";
+@GetMapping("/candidatos")
+public String getListaAlumnos(Model model) {
+	listaCandidato listaAlumnos = new listaCandidato();
+	model.addAttribute("alumnos", listaAlumnos.getCandidatos());
+	return "lista_alumnos";
 }
-
-
 }
