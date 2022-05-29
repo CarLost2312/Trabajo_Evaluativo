5b438b2cfedaaef31ef28d689eeea364ee200d0a
Enamuel L <Enamuel L@EmAnUeL>
2022-05-29 13:53:47 -0300

    cree lista usuario y candidato

---
+++ Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/util/listaCandidato.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.util;
+
+import java.util.ArrayList;
+
+import ar.edu.unju.fi.model.Candidato;
+
+public class listaCandidato {
+	ArrayList<Candidato> listaCandidato= new ArrayList<Candidato>();
+}
