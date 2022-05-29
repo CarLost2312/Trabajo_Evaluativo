1b2af9976c3ee669f0fb6ae323f7b93eba54406c
PC <PC@MEGATRON>
2022-05-29 14:34:46 -0300

    controller

--- Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/controller/CandidatosController.java
+++ Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/controller/CandidatosController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UsuarioController {
+public class CandidatosController {
 
-@GetMapping("/Usuarios")
+@GetMapping("/Candidatos")
 public String getIndexPage() {
 	return "Candidatos";
 }
