33126b708b980ac34639ced73b31fb12177ac340
orochimeii <babymeyfong@gmail.com>
2022-05-30 19:46:43 -0300

    q

--- Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/controller/EstadisticaController.java
+++ Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/controller/EstadisticaController.java
@@ -1,13 +1,20 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.edu.unju.fi.service.ICandidatoService;
 
 @Controller
 public class EstadisticaController {
-	
+	@Autowired
+	private ICandidatoService candidatoService;
+
 @GetMapping("/estadistica")	
-public String getEstadisticaPage () {
+public String getEstadisticaPage (Model model) {
+	model.addAttribute("estadistica", candidatoService.getListaCandidato().getListaCandidato());
 return "estadistica";	
 	
 }
