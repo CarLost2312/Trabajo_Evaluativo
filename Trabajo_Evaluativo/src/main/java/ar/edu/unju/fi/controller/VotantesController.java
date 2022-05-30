f69fcfdb2b78574293dc21af0eace1bcafdbaf14
orochimeii <babymeyfong@gmail.com>
2022-05-30 20:43:27 -0300

    votantes tabla

--- Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/controller/VotantesController.java
+++ Trabajo_Evaluativo/src/main/java/ar/edu/unju/fi/controller/VotantesController.java
@@ -1,13 +1,21 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.edu.unju.fi.service.IUsuarioService;
 
 @Controller
 public class VotantesController {
+	@Autowired
+	private IUsuarioService usuarioService;
 @GetMapping ("/votantes")	
-public String getVotantesPage() {
-	return "votantes";
-}
+public String getEstadisticaPage (Model model) {
+	model.addAttribute("listausuario", usuarioService.getListaUsuario().getListaUsuario());
 
+return "votantes";
+
+}
 }
